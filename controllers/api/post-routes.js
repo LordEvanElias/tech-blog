9ca857b647a83f2f7a9dd0b2b3c917fa815821f6
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-01-31 18:29:56 -0600

    Added a post route and started working on the main.handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
-const Dish = require("../../models/Post");
+const Post = require("../../models/Post");
 
-// route to create/add a dish using async/await
-
-// if the dish is successfully created, the new response will be returned as json
+router.post("/", async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
